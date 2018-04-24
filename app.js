2915d6821a3c885ed073026e4304eece30f61699
Julien <julien.chenel@gmail.com>
2018-04-24 23:56:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("#thomas").click(function() {
+
+        $("#XMLID_10_").html("<stop stop-color='#2DB3D9' offset='0'/><stop stop-color='#2DB3D9' offset='1'/>");
+
+        $("#XMLID_12_").html("<stop stop-color='#2DB3D9' offset='0'/><stop stop-color='#2DB3D9' offset='1'/>");
+
+        $("#cheveux").attr("fill", "#FFDC8F");
+
+        $("#idGreg").hide();
+        $("#idThom").show();
+
+
+    })
+
+    $("#gregoire").click(function() {
+
+        $("#XMLID_10_").html("<stop stop-color='#0F9225' offset='0'/><stop stop-color='#0F9225' offset='1'/>");
+
+        $("#XMLID_12_").html("<stop stop-color='#0F9225' offset='0'/><stop stop-color='#0F9225' offset='1'/>");
+
+        $("#cheveux").attr("fill", "#8D6304");
+
+        $("#idThom").hide();
+        $("#idGreg").show();
+    })
+
+
+});
+
+// Blue : #2DB3D9
+// Green : #0F9225
+// Blond : #FFDC8F
+// Chatain : #8D6304
